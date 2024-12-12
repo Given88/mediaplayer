645cbe8b166506aa31b70b7d2e5bcdfa61778b45
Given Mahlangu <gtshepo276@gmail.com>
2024-12-13 01:56:09 +0200

    The application plays an audio from the Resource folder(raw) and it supports play and pause playback options.

--- app/src/main/java/com/gtworld/simplemediaplayer/MainActivity.java
+++ app/src/main/java/com/gtworld/simplemediaplayer/MainActivity.java
@@ -1,6 +1,10 @@
 package com.gtworld.simplemediaplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private TextView resView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        resView = findViewById(R.id.resView);
+        mediaPlayer = MediaPlayer.create(this, R.raw.we_are_the_world);
+        //used MediaPlayer.create(this, R.raw.we_are_the_world) because it allows a faster simple access to the resource
+        //but it has limitations which I cannot fully configure the data file
+        //no need to prepare the audio
+    }
+
+    public void playbtnClicked(View view) {
+       //Start playing the music
+mediaPlayer.start();//start playing
+resView.setText("Playing...");
+
+    }
+    public void pausebtnClicked(View view) {
+       //pauses the music
+mediaPlayer.pause();
+        resView.setText("Paused...");
     }
 }
